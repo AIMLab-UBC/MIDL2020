6cdac132527c5efdc30a51f691b599469b7b563a
AliBashashati <yiping@wang.vision>
2020-01-25 21:05:06 -0800

    .

--- utils/utils.py
+++ utils/utils.py
@@ -7,9 +7,9 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import f1_score
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import confusion_matrix
-import glob
 import numpy as np
 import matplotlib.path as pltPath
+import glob
 import re
 import os
 import random
