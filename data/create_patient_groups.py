c3fa31d8127ebe16ca545c8be103ebee2d10e972
AliBashashati <yiping@wang.vision>
2020-04-15 19:27:48 -0700

    .

--- data/create_patient_groups.py
+++ data/create_patient_groups.py
@@ -13,6 +13,7 @@ import sys
 import os.path
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+import utils.utils as utils
 
 PATIENT_REGEX = utils.PATIENT_REGEX
 
